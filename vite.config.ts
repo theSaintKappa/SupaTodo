88f692cb7cba3194236b03e347b02d4dab1c4cc8
SaintKappa <00wojtekz@gmail.com>
2024-03-02 05:08:43 +0100

    💜 Add Million.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from "@vitejs/plugin-react";
+import million from "million/compiler";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+    plugins: [million.vite({ auto: true }), react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
