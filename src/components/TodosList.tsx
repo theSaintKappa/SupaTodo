19067a3cbff13584ea0a5bfd1a98d0ca1a9c606b
Wojtek Zrałek <00wojtekz@gmail.com>
2024-03-16 19:55:55 +0100

    ☑️ Update `TodosList.tsx`

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,37 +1,48 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import supabase from "@/supabase";
 import type { Tables } from "@/types/db.types";
 import { cn } from "@/utils/cn";
 import { CheckCheck, SquarePen, Trash2 } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 
+function setCompleted(id: number, completed: boolean) {
+    supabase.from("todos").update({ completed }).eq("id", id).then(console.log);
+}
+
+function deleteTodo(id: number) {
+    supabase.from("todos").delete().eq("id", id).then(console.log);
+}
+
 export function TodosList({ todos }: { todos: Tables<"todos">[] }) {
-    const priorityColors = ["green-500", "yellow-300", "red-600"];
+    const borderColors = ["border-green-500", "border-yellow-300", "border-red-600"];
     const priorityText = ["Low", "Medium", "High"];
 
     return (
         <div className="flex flex-col gap-4 py-4">
             {todos.map((todo) => (
-                <Card key={todo.id} className={`flex items-center justify-between p-4 border-2 border-opacity-90 border-${priorityColors[todo.priority - 1]}`}>
-                    <CardHeader className="p-0">
+                <Card key={todo.id} className={cn("flex items-center justify-between p-4 border-2 border-opacity-75", borderColors[todo.priority - 1])}>
+                    <CardHeader className={cn("p-0", todo.completed && "opacity-75")}>
                         <CardTitle className={cn("flex items-center gap-2", todo.completed && "line-through")}>
                             {todo.title}
-                            <Badge className={`border-2 border-opacity-90 border-${priorityColors[todo.priority - 1]}`}>{priorityText[todo.priority - 1]}</Badge>
+                            <Badge variant="outline" className={cn("border-2 border-opacity-75", borderColors[todo.priority - 1])}>
+                                {priorityText[todo.priority - 1]}
+                            </Badge>
                         </CardTitle>
                         {todo.description && <CardDescription>{todo.description}</CardDescription>}
                     </CardHeader>
                     <CardContent className="p-0 flex gap-2">
                         {!todo.completed ? (
                             <Button variant="outline">
-                                <SquarePen />
+                                <SquarePen className="h-5 min-w-5" />
                             </Button>
                         ) : (
-                            <Button variant="destructive">
-                                <Trash2 />
+                            <Button variant="destructive" onClick={() => deleteTodo(todo.id)}>
+                                <Trash2 className="h-5 min-w-5" />
                             </Button>
                         )}
-                        <Button>
-                            <CheckCheck />
+                        <Button onClick={() => setCompleted(todo.id, !todo.completed)}>
+                            <CheckCheck className="h-5 min-w-5" />
                         </Button>
                     </CardContent>
                 </Card>
